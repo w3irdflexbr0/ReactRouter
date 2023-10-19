ccc85b4c2694132933f3ba92aed50a5eb1d87608
Tanvir Singh <thedeathcoredude@gmail.com>
2023-10-19 16:59:29 -0400

    swerve

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,28 @@
+import React from 'react'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Routes, Route, Link } from 'react-router-dom';
-
+import { Routes, Route, Link } from 'react-router-dom'
+import Red from './components/Red'
+import Blue from './components/Blue'
+import Home from './components/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div id="container">
-    <h1>Hello React Router!</h1>
-    <div id="navbar">{/* navigation here */}</div>
-    <div id="main-section">{/* routes here */}</div>
-  </div>
-  <div id="main-section">
-   <Routes>
-     <Route path="/blue" element={<h1>Blue</h1>} />
-     <Route path="/red" element={<h1>Red</h1>} />
-   </Routes>
-</div>
-</div>
+      <div id="navbar">
+        <Link to="/red">Red</Link>
+        <Link to="/blue">Blue</Link>
+        <Link to="/">Home</Link>
+      </div>
+      <div id="main-section">
+        <Routes>
+          <Route path="/red" element={<Red />} /> {/* Use Red component */}
+          <Route path="/blue" element={<Blue />} /> {/* Use Blue component */}
+          <Route path="/" element={<Home />} /> {}
+        </Routes>
+      </div>
+    </div>
   )
 }
 
